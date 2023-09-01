3352c62d5c65824cdc1664d0de4d56efe503741a
Lenkoras <kernelsubject@gmail.com>
2023-09-01 14:58:11 +0300

    Improved documentation of methods, readme and updated package version.

--- README.md
+++ README.md
@@ -5,12 +5,16 @@ Extensions designed to add all implementations of an interface or class from a s
 
 Quick example:
 ```csharp
+using Microsoft.Extensions.DependencyInjection;
+using Siva.Extensions.DependencyInjection;
+
+...
 public void ConfigureServices(IServiceCollection services)
 {
     services.AddImplementations(typeof(IRepository<>), typeof(Program).Assembly);
 }
 ```
-This command will add all `IRepository<>` implementations to the `ServiceCollection` from the specified assembly.
+This command will add all `IRepository<T>` implementations to the `ServiceCollection` from the specified assembly.
 
 For example, here the `UserRepository` class will be added as an implementation type and `IUserRepository` as a service type:
 ```csharp
@@ -21,19 +25,32 @@ public interface IRepository<T>
 
 public interface IUserRepository : IRepository<User>
 {
-    User? GetByNameAsync(string name);
+    Task<User?> GetByNameAsync(string name);
 }
 
 public class UserRepository : IUserRepository
 {
-    public User? GetByNameAsync(string name)
+    public Task<User?> GetByNameAsync(string name)
     { 
         ...
     }
 }
 ```
+And you can use this in your services as follows:
+```csharp
+public class CurrentUserService
+{
+    private readonly IUserRepository userRepository;
 
-Or you can also use a class as a base type:
+    public CurrentUserService(IUserRepository userRepository) 
+    {
+        this.userRepository = userRepository;
+    }
+}
+```
+In the future, when we add another public service implementing this interface (for example, `BookRepository` and `IBookRepository`), we won't need to manually add it to the collection of services.
+
+Need to add a class as a service type? No problem, you can do it like this:
 ```csharp
 public void ConfigureServices(IServiceCollection services)
 {
@@ -64,6 +81,23 @@ public class TrainFactory : Factory<Train>
 }
 ```
 And here, the `ShoeFactory` and `TrainFactory` types will be added as implementations, and the `Factory<Shoe>` and `Factory<Train>` types will act as service types, respectively.
+
+By default, the lifetime of the added services is `ServiceLifetime.Scoped`, but if necessary, you can set any other as follows:
+```csharp
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddImplementations(typeof(Factory<>), typeof(Program).Assembly, ServiceLifetime.Singleton);
+}
+```
+
+For specific scenarios, you can also add service implementations directly from the type collection:
+```csharp
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddImplementations(typeof(IRepository<>), typeof(Program).Assembly.GetTypes().Where(type => type.IsNotPublic));
+}
+```
+In the example above, all non-public types that implement the `IRepository<T>` interface will be added.
 
 # Restrictions
 
--- Siva.Extensions.DependencyInjection.Implementations/ImplementationInjectionExtensions.cs
+++ Siva.Extensions.DependencyInjection.Implementations/ImplementationInjectionExtensions.cs
@@ -7,37 +7,38 @@ using System.Reflection;
 namespace Siva.Extensions.DependencyInjection
 {
     /// <summary>
-    /// An extension class designed to add all implementations of an interface or class from a sequence of types or assembly.
+    /// An extension class designed to add all implementations of an interface or class from a sequence of types or assembly to the <see cref="IServiceCollection"/>.
     /// </summary>
     public static partial class ImplementationInjectionExtensions
     {
         /// <summary>
-        /// Adds all unique implementations of the <paramref name="serviceTypeToScan"/> to the collection of <paramref name="services"/>.
-        /// If the <paramref name="serviceTypeToScan"/> is an interface, 
-        /// then the service type will be the first interface that is assignable to the <paramref name="serviceTypeToScan"/> or this interface itself.
-        /// If the <paramref name="serviceTypeToScan"/> is a class, then the service type will be its implementation.
-        /// The types of implementations will be taken from the specified <paramref name="typesToScan"/>.
+        /// Selects all unique implementation types of the <paramref name="targetAssignableType"/> 
+        /// from the specified collection of <paramref name="types"/> 
+        /// and adds them to the collection of <paramref name="services"/>.
+        /// If the <paramref name="targetAssignableType"/> is an interface, 
+        /// then the service type will be the first interface that is assignable to the <paramref name="targetAssignableType"/> or this interface itself.
+        /// If the <paramref name="targetAssignableType"/> is a class, then the service type will be its implementation.
         /// </summary>
         /// <param name="services">Collection of the <see cref="ServiceDescriptor"/>s.</param>
-        /// <param name="serviceTypeToScan">Any non-sealed type.</param>
-        /// <param name="typesToScan">Types to scan.</param>
+        /// <param name="targetAssignableType">Any non-sealed type that will be searched for among the <paramref name="types"/>.</param>
+        /// <param name="types">Types to scan.</param>
         /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service.</param>
         /// <returns>The same service collection to continue the call chain.</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="ArgumentException">Throws an exception if <see cref="Type.IsSealed"/> for <paramref name="serviceTypeToScan"/> returns <see langword="true"/>.</exception>
-        public static IServiceCollection AddImplementations(this IServiceCollection services, Type serviceTypeToScan, IEnumerable<Type> typesToScan, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        /// <exception cref="ArgumentException">Throws an exception if <see cref="Type.IsSealed"/> for <paramref name="targetAssignableType"/> returns <see langword="true"/>.</exception>
+        public static IServiceCollection AddImplementations(this IServiceCollection services, Type targetAssignableType, IEnumerable<Type> types, ServiceLifetime lifetime = ServiceLifetime.Scoped)
         {
             ArgumentNullException.ThrowIfNull(services);
-            ArgumentNullException.ThrowIfNull(serviceTypeToScan);
-            ArgumentNullException.ThrowIfNull(typesToScan);
+            ArgumentNullException.ThrowIfNull(targetAssignableType);
+            ArgumentNullException.ThrowIfNull(types);
 
-            IServiceTypeProvider serviceTypeProvider = new ServiceTypeProviderFactory().Create(serviceTypeToScan);
+            IServiceTypeProvider serviceTypeProvider = new ServiceTypeProviderFactory().Create(targetAssignableType);
 
             HashSet<Type> setOfUsedServiceTypes = new();
 
-            IEnumerable<Type> types = typesToScan.Where(IsNotValueImplementationType);
+            IEnumerable<Type> implementationTypes = types.Where(IsNotValueImplementationType);
 
-            foreach (Type implementationType in types)
+            foreach (Type implementationType in implementationTypes)
             {
                 if (serviceTypeProvider.TryProvide(implementationType, out Type? serviceType) &&
                     setOfUsedServiceTypes.Add(serviceType)) // if this type of service is not already in use, then adds to the service collection
@@ -53,47 +54,49 @@ namespace Siva.Extensions.DependencyInjection
         }
 
         /// <summary>
-        /// Adds all unique implementations of the <paramref name="serviceTypeToScan"/> to the collection of <paramref name="services"/>.
-        /// If the <paramref name="serviceTypeToScan"/> is an interface, 
-        /// then the service type will be the first interface that is assignable to the <paramref name="serviceTypeToScan"/> or this interface itself.
-        /// If the <paramref name="serviceTypeToScan"/> is a class, then the service type will be its implementation.
-        /// The types to search for public implementations will be taken from the specified <paramref name="assembly"/>.
+        /// Selects all unique implementation types of the <paramref name="targetAssignableType"/>
+        /// that are taken from the specified <paramref name="assembly"/>
+        /// and adds them to the collection of <paramref name="services"/>.
+        /// If the <paramref name="targetAssignableType"/> is an interface,
+        /// then the service type will be the first interface that is assignable to the <paramref name="targetAssignableType"/> or this interface itself.
+        /// If the <paramref name="targetAssignableType"/> is a class, then the service type will be its implementation.
         /// </summary>
         /// <param name="services">Collection of the <see cref="ServiceDescriptor"/>s.</param>
-        /// <param name="serviceTypeToScan">Any non-sealed type.</param>
-        /// <param name="assembly">Assembly to scan.</param>
+        /// <param name="targetAssignableType">Any non-sealed type that will be searched for among the exported types.</param>
+        /// <param name="assembly">Assembly to scan. Takes all exported types from this assembly.</param>
         /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service.</param>
         /// <returns>The same service collection to continue the call chain.</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="ArgumentException">Throws an exception if <see cref="Type.IsSealed"/> for <paramref name="serviceTypeToScan"/> returns <see langword="true"/>.</exception>
-        public static IServiceCollection AddImplementations(this IServiceCollection services, Type serviceTypeToScan, Assembly assembly, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        /// <exception cref="ArgumentException">Throws an exception if <see cref="Type.IsSealed"/> for <paramref name="targetAssignableType"/> returns <see langword="true"/>.</exception>
+        public static IServiceCollection AddImplementations(this IServiceCollection services, Type targetAssignableType, Assembly assembly, ServiceLifetime lifetime = ServiceLifetime.Scoped)
         {
             ArgumentNullException.ThrowIfNull(assembly);
 
-            return AddImplementations(services, serviceTypeToScan, assembly.GetExportedTypes(), lifetime);
+            return AddImplementations(services, targetAssignableType, assembly.GetExportedTypes(), lifetime);
         }
 
         /// <summary>
-        /// Adds all unique implementations of the <paramref name="serviceTypeToScan"/> to the collection of <paramref name="services"/>.
-        /// If the <paramref name="serviceTypeToScan"/> is an interface, 
-        /// then the service type will be the first interface that is assignable to the <paramref name="serviceTypeToScan"/> or this interface itself.
-        /// If the <paramref name="serviceTypeToScan"/> is a class, then the service type will be its implementation.
-        /// The types to search for public implementations will be taken from the specified <paramref name="assemblies"/>.
+        /// Selects all unique implementation types of the <paramref name="targetAssignableType"/>
+        /// that are taken from the specified <paramref name="assemblies"/> and 
+        /// adds them to the collection of <paramref name="services"/>.
+        /// If the <paramref name="targetAssignableType"/> is an interface, 
+        /// then the service type will be the first interface that is assignable to the <paramref name="targetAssignableType"/> or this interface itself.
+        /// If the <paramref name="targetAssignableType"/> is a class, then the service type will be its implementation.
         /// </summary>
         /// <param name="services">Collection of the <see cref="ServiceDescriptor"/>s.</param>
-        /// <param name="serviceTypeToScan">Any non-sealed type.</param>
-        /// <param name="assemblies">The sequence of assembly to scan.</param>
+        /// <param name="targetAssignableType">Any non-sealed type that will be searched for among the exported types.</param>
+        /// <param name="assemblies">Assemblies to scan. Takes all exported types from these assemblies.</param>
         /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service.</param>
         /// <returns>The same service collection to continue the call chain.</returns>
         /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="ArgumentException">Throws an exception if <see cref="Type.IsSealed"/> for <paramref name="serviceTypeToScan"/> returns <see langword="true"/>.</exception>
-        public static IServiceCollection AddImplementations(this IServiceCollection services, Type serviceTypeToScan, IEnumerable<Assembly> assemblies, ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        /// <exception cref="ArgumentException">Throws an exception if <see cref="Type.IsSealed"/> for <paramref name="targetAssignableType"/> returns <see langword="true"/>.</exception>
+        public static IServiceCollection AddImplementations(this IServiceCollection services, Type targetAssignableType, IEnumerable<Assembly> assemblies, ServiceLifetime lifetime = ServiceLifetime.Scoped)
         {
             ArgumentNullException.ThrowIfNull(assemblies);
 
             foreach (Assembly assembly in assemblies)
             {
-                AddImplementations(services, serviceTypeToScan, assembly, lifetime);
+                AddImplementations(services, targetAssignableType, assembly, lifetime);
             }
 
             return services;
